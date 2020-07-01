5c755edb2792557833f49e39838e69be89b54111
ACLDise <67666036+ACLDise@users.noreply.github.com>
2020-06-30 19:27:56 -0500

    Add files via upload

---
+++ configs/funcion.php
@@ -0,0 +1,5 @@
+<?php
+ mysql_connect($host_mysql,$user_mysql,$pass_mysql) or die("Error conectamdo a la base de datos");
+
+ mysql_select_db($db_mysql) or die("Error conectamdo a la base de datos");
+?>
